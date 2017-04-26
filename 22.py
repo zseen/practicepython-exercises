0684fe315ea6675d8ef1a00a267a67650773b502
zseen <43151120+zseen@users.noreply.github.com>
2017-04-26 20:41:23 +0100

    Ex22 - Use camelCase

--- 22.py
+++ 22.py
@@ -1,12 +1,12 @@
-counter_dict = {}
+counterDict = {}
 with open('nameslist.txt') as f:
     line = f.readline()
     while line:
         line = line.strip()
-        if line in counter_dict:
-            counter_dict[line] += 1
+        if line in counterDict:
+            counterDict[line] += 1
         else:
-            counter_dict[line] = 1
+            counterDict[line] = 1
         line = f.readline()
 
-print(counter_dict)
+print(counterDict)
