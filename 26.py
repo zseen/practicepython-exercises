44adcc3b4a7e64d2fe9b3093f07383913c7f747c
zseen <43151120+zseen@users.noreply.github.com>
2017-05-01 18:05:59 +0100

    Ex 26 - Refactor functions vertical and diagonal, add unit test

--- 26.py
+++ 26.py
@@ -28,28 +28,41 @@ def horizontalWin(game):
 
     return NOBODY_WINS
 
-
 def verticalWin(game):
-    if game[0][0]==game[1][0]==game[2][0]==1 or game[0][1]==game[1][1]==game[2][1]==1 or game[0][2]==game[1][2]==game[2][2]==1:
-        return FIRST_WINS
+    N = len(game)
 
-    elif game[0][0]==game[1][0]==game[2][0]==2 or game[0][1]==game[1][1]==game[2][1]==2 or game[0][2]==game[1][2]==game[2][2]==2:
-        return SECOND_WINS
-    else:
-        return NOBODY_WINS
+    for i in range(0, N):
+        rowList = []
+        for j in range(0, N):
+            rowList.append(game[j][i])
+
+        if (all_same(rowList)) and rowList[0] != 0:
+            return numberToWinner(rowList[0])
+
+    return NOBODY_WINS
 
 def diagonalWin(game):
-    if game[0][0]==game[1][1]==game[2][2]==1:
-        return FIRST_WINS
-    elif game[0][0] == game[1][1] == game[2][2] == 2:
-        return SECOND_WINS
+    N = len(game)
 
-    if game[0][2]==game[1][1]==game[2][0]==1:
-        return FIRST_WINS
-    elif game[0][2] == game[1][1] == game[2][0] == 2:
-        return SECOND_WINS
-    else:
-        return NOBODY_WINS
+
+    rowList = []
+    for i in range(0, N):
+        rowList.append(game[i][i])
+
+    if (all_same(rowList)) and rowList[0] != 0:
+        return numberToWinner(rowList[0])
+
+
+    rowList = []
+    for i in range(0, N):
+        rowList.append(game[i][N-1-i])
+
+    if (all_same(rowList)) and rowList[0] != 0:
+        return numberToWinner(rowList[0])
+
+    return NOBODY_WINS
+
+
 
 def main():
     game = [[2, 1, 1],
@@ -64,6 +77,8 @@ def main():
         print ("No one won")
 
 class TestWinChecking(unittest.TestCase):
+
+    # ---- Horizontal win ----
 
     def test_HorizontalWin_FirstLineAllOnes_FirstPlayerWins(self):
         game = [[1, 1, 1],
@@ -99,6 +114,89 @@ class TestWinChecking(unittest.TestCase):
                 [2, 2, 2]]
         horizWinResult = horizontalWin(game)
         self.assertTrue(horizWinResult == SECOND_WINS)
+
+    # ---- Vertical win ----
+
+    def test_VerticalWin_FirstColumnAllOnes_FirstPlayerWins(self):
+        game = [[1, 0, 0],
+                [1, 0, 0],
+                [1, 2, 2]]
+        verticWinResult = verticalWin(game)
+        self.assertTrue(verticWinResult == FIRST_WINS)
+
+    def test_VerticalWin_SecondColumnAllOnes_FirstPlayerWins(self):
+        game = [[2, 1, 0],
+                [0, 1, 0],
+                [0, 1, 2]]
+        verticWinResult = verticalWin(game)
+        self.assertTrue(verticWinResult == FIRST_WINS)
+
+    def test_VerticalWin_ThirdColumnAllTwos_SecondPlayerWins(self):
+        game = [[2, 1, 2],
+                [0, 0, 2],
+                [0, 1, 2]]
+        verticWinResult = verticalWin(game)
+        self.assertTrue(verticWinResult == SECOND_WINS)
+
+    def test_VerticalWin_RandomLines_NoOneWins(self):
+        game = [[2, 1, 2],
+                [0, 0, 1],
+                [0, 1, 2]]
+        verticWinResult = verticalWin(game)
+        self.assertTrue(verticWinResult == NOBODY_WINS)
+
+    def test_VerticalWin_ThirdColumnAllOnes_FirstPlayerWins(self):
+        game = [[2, 1, 1],
+                [0, 0, 1],
+                [0, 1, 1]]
+        verticWinResult = verticalWin(game)
+        self.assertTrue(verticWinResult == FIRST_WINS)
+
+    # ---- Diagonal win ----
+
+    def test_DiagonalWin_FirstUpperCornerOnes_FirstPlayerWins(self):
+        game = [[1, 0, 2],
+                [0, 1, 1],
+                [0, 1, 1]]
+        diagWinResult = diagonalWin(game)
+        self.assertTrue(diagWinResult == FIRST_WINS)
+
+    def test_DiagonalWin_FirstUpperCornerTwos_SecondPlayerWins(self):
+        game = [[2, 0, 2],
+                [0, 2, 1],
+                [0, 1, 2]]
+        diagWinResult = diagonalWin(game)
+        self.assertTrue(diagWinResult == SECOND_WINS)
+
+    def test_DiagonalWin_SecondUpperCornerOnes_FirstPlayerWins(self):
+        game = [[2, 0, 1],
+                [0, 1, 1],
+                [1, 1, 2]]
+        diagWinResult = diagonalWin(game)
+        self.assertTrue(diagWinResult == FIRST_WINS)
+
+    def test_DiagonalWin_SecondUpperCornerTwos_SecondPlayerWins(self):
+        game = [[2, 0, 2],
+                [0, 2, 1],
+                [2, 1, 1]]
+        diagWinResult = diagonalWin(game)
+        self.assertTrue(diagWinResult == SECOND_WINS)
+
+
+
+    def test_DiagonalWin_RandomLines_NoOneWins(self):
+        game = [[2, 0, 1],
+                [0, 0, 1],
+                [1, 1, 2]]
+        diagWinResult = diagonalWin(game)
+        self.assertTrue(diagWinResult == NOBODY_WINS)
+
+
+
+
+
+
+
 
 if __name__=="__main__":
     unittest.main()
