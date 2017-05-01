d950323e4419db4d080a70698496093c31da23a6
zseen <43151120+zseen@users.noreply.github.com>
2017-05-01 16:31:30 +0100

    Ex 26 - Remove comments and add unit tests

--- 26.py
+++ 26.py
@@ -1,78 +1,4 @@
-# game = [[2, 1, 1],
-# 	    [2, 1, 2],
-# 	    [2, 2, 1]]
-#
-# firstWins="First player wins"
-# secondWins="Second player wins"
-# noneWins="Nobody won"
-
-# def horizontalWin(game):
-#     if game[0][0]==game[0][1]==game[0][2] or game[1][0]==game[1][1]==game[1][2] or game[2][0]==game[2][1]==game[2][2]:
-#         if game[0][0] == 1 and game[0][1] == 1 and game[0][2] == 1:
-#             return firstWins
-#         elif game [1][0] == 1 and game[1][1] == 1 and game[1][2] == 1:
-#             return firstWins
-#         elif game [2][0] == 1 and game[2][1] == 1 and game[2][2] == 1:
-#             return firstWins
-#
-#         elif game[0][0] == 2 and game[0][1] == 2 and game[0][2] == 2:
-#             return secondWins
-#         elif game [1][0] == 2 and game[1][1] == 2 and game[1][2] == 2:
-#             return secondWins
-#         elif game [2][0] == 2 and game[2][1] == 2 and game[2][2] == 2:
-#             return secondWins
-#     else:
-#         return noneWins
-#
-# def verticalWin(game):
-#     if game[0][0]==game[1][0]==game[2][0] or game[0][1]==game[1][1]==game[2][1] or game[0][2]==game[1][2]==game[2][2]:
-#         if game [0][0] == 1 and game[1][0] == 1 and game[2][0] == 1:
-#             return firstWins
-#         elif game [0][1] == 1 and game[1][1] == 1 and game[2][1] == 1:
-#             return firstWins
-#         elif game [0][2]== 1 and game[1][2] == 1 and game[2][2] == 1:
-#             return firstWins
-#
-#         elif game [0][0] == 2 and game[1][0] == 2 and game[2][0] == 2:
-#             return secondWins
-#         elif game [0][1] == 2 and game[1][1] == 2 and game[2][1] == 2:
-#             return secondWins
-#         elif game [0][2] == 2 and game[1][2] == 2 and game[2][2] == 2:
-#             return secondWins
-#     else:
-#         return noneWins
-#
-# def diagonalWin(game):
-#     if game[0][0]==game[1][1]==game[2][2]:
-#         if game[0][0] == 1:
-#             return firstWins
-#         elif game[0][0] == 2:
-#             return secondWins
-#     if game[0][2]==game[1][1]==game[2][0]:
-#         if game[0][2] == 1:
-#             return firstWins
-#         elif game[0][2] == 2:
-#             return secondWins
-#     else:
-#         return noneWins
-
-# def main():
-#     if horizontalWin(game)== firstWins or verticalWin(game)== firstWins or diagonalWin(game) == firstWins:
-#         print ("The first player won")
-#     elif horizontalWin(game)== secondWins or verticalWin(game)== secondWins or diagonalWin(game) == secondWins:
-#         print ("The second player won")
-#     else:
-#         print("No one won")
-#
-# if __name__=="__main__":
-#     main()
-
-
-####TRY
-
-game = [[2, 1, 1],
-	    [1, 2, 2],
-	    [1, 2, 2]]
+import unittest
 
 firstWins="First player wins"
 secondWins="Second player wins"
@@ -81,7 +7,6 @@ noneWins="Nobody won"
 def horizontalWin(game):
     if game[0][0]==game[0][1]==game[0][2]==1 or game[1][0]==game[1][1]==game[1][2]==1 or game[2][0]==game[2][1]==game[2][2]==1:
         return firstWins
-
     elif game[0][0]==game[0][1]==game[0][2]==2 or game[1][0]==game[1][1]==game[1][2]==2 or game[2][0]==game[2][1]==game[2][2]==2:
         return secondWins
     else:
@@ -110,32 +35,53 @@ def diagonalWin(game):
         return noneWins
 
 def main():
+    game = [[2, 1, 1],
+            [1, 2, 2],
+            [1, 2, 2]]
+
     if horizontalWin(game)== firstWins or verticalWin(game)== firstWins or diagonalWin(game) == firstWins:
         print ("The first player won")
     elif horizontalWin(game)== secondWins or verticalWin(game)== secondWins or diagonalWin(game) == secondWins:
         print ("The second player won")
     else:
-        print("No one won")
+        print ("No one won")
+
+class TestWinChecking(unittest.TestCase):
+
+    def test_HorizontalWin_FirstLineAllOnes_FirstPlayerWins(self):
+        game = [[1, 1, 1],
+                [0, 0, 0],
+                [0, 0, 0]]
+        horizWinResult = horizontalWin(game)
+        self.assertTrue(horizWinResult == firstWins)
+
+    def test_HorizontalWin_SecondLineAllOnes_FirstPlayerWins(self):
+        game = [[0, 0, 0],
+                [1, 1, 1],
+                [0, 0, 0]]
+        horizWinResult = horizontalWin(game)
+        self.assertTrue(horizWinResult == firstWins)
+
+    def test_HorizontalWin_ThirdLineAllOnes_FirstPlayerWins(self):
+        game = [[0, 0, 0],
+                [0, 0, 0],
+                [1, 1, 1]]
+        horizWinResult = horizontalWin(game)
+        self.assertTrue(horizWinResult == firstWins)
+
+    def test_NoWins_RandomLines_NoOneWins(self):
+        game = [[0, 1, 0],
+                [2, 0, 0],
+                [1, 2, 1]]
+        horizWinResult = horizontalWin(game)
+        self.assertTrue(horizWinResult == noneWins)
+
+    def test_HorizontalWin_ThirdLineAllTwos_SecondPlayerWins(self):
+        game = [[0, 0, 0],
+                [0, 0, 0],
+                [2, 2, 2]]
+        horizWinResult = horizontalWin(game)
+        self.assertTrue(horizWinResult == secondWins)
 
 if __name__=="__main__":
-    main()
-
-
-
-
-# if verticalWin(game)==firstWins:
-#     print ("a")
-# elif verticalWin(game)==secondWins:
-#     print ("b")
-# if horizontalWin(game)==firstWins:
-#     print ("c")
-# elif horizontalWin(game)==secondWins:
-#     print ("d")
-#
-# if diagonalWin(game)==firstWins:
-#     print ("e")
-# elif diagonalWin(game)==secondWins:
-#     print ("f")
-
-
-
+    unittest.main()
