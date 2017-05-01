7cc9ddb29324dfdfb5ca083aae19199e366d56ce
zseen <43151120+zseen@users.noreply.github.com>
2017-05-01 14:20:33 +0100

    Ex24 - Modify the board (organising the whitespaces)
    Also reformat the code.

--- 24.py
+++ 24.py
@@ -1,18 +1,16 @@
 
-def gameBoard(x):
-    for lines in range (1,x+1):
-        print ("---  " *x)
-        print ("|   " *x + "|")
-    print ("---  " *x)
+def gameBoard(lines):
+    for _ in range(1, lines+1):
+        print(" ---" * lines)
+        print("|   " * lines + "|")
+    print(" ---" * lines)
 
 
 
 def main():
-    x = int(input("What size would you like?"))
-    gameBoard(x)
+    size = int(input("What size would you like?"))
+    gameBoard(size)
 
 
 if __name__=="__main__":
     main()
-
-    
