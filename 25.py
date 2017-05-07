311dfde56e0a7c2489e0f4f4baf23465d6727b38
zseen <43151120+zseen@users.noreply.github.com>
2017-05-07 16:14:24 +0100

    Ex 25 - Refactor logic to class

--- 25.py
+++ 25.py
@@ -1,27 +1,35 @@
-possibilities = []
+class Guesser(object):
+    def __init__(self):
+        self.possibilities=[]
+        for i in range(0, 101):
+            self.possibilities.append(i)
+    
+    def play(self):
+        inputBegin = input("Think of a number please, when ready, write 'ready' and I start guessing!:")
+        if inputBegin == "ready":
+            counter = 0
+            while True:
+                counter += 1
+
+                middle = len(self.possibilities) // 2
+                numberGuessed = self.possibilities[middle]
+
+                guessingResult = input(
+                    "My guess is %s. If right, write 'right', if too low, write 'low', if too high, write 'high':" % str(
+                        numberGuessed))
+                if guessingResult == "right":
+                    print("Hooray")
+                    print("It took me %s guesses" % str(counter))
+                    break
+                elif guessingResult == "low":
+                    del self.possibilities[0:middle]
+                elif guessingResult == "high":
+                    del self.possibilities[middle:]
+        
 
 def main():
-    for i in range(0, 101):
-        possibilities.append(i)
-
-    inputBegin=input("Think of a number please, when ready, write 'ready' and I start guessing!:")
-    if inputBegin == "ready":
-        counter = 0
-        while True:
-            counter += 1
-
-            middle = len(possibilities)//2
-            numberGuessed = possibilities[middle]
-
-            guessingResult = input("My guess is %s. If right, write 'right', if too low, write 'low', if too high, write 'high':" % str(numberGuessed))
-            if guessingResult == "right":
-                print("Hooray")
-                print ("It took me %s guesses" % str(counter))
-                break
-            elif guessingResult == "low":
-                del possibilities[0:middle]
-            elif guessingResult == "high":
-                del possibilities[middle:]
+    guesser = Guesser()
+    guesser.play()
 
 if __name__=="__main__":
     main()
