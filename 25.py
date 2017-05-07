d4eee28787a416dcd053044247b81561ee735a82
zseen <43151120+zseen@users.noreply.github.com>
2017-05-07 15:32:19 +0100

    Ex 25 - Add exercise 25

---
+++ 25.py
@@ -0,0 +1,27 @@
+possibilities = []
+
+def main():
+    for i in range(0, 101):
+        possibilities.append(i)
+
+    inputBegin=input("Think of a number please, when ready, write 'ready' and I start guessing!:")
+    if inputBegin == "ready":
+        counter = 0
+        while True:
+            counter += 1
+
+            middle = len(possibilities)//2
+            numberGuessed = possibilities[middle]
+
+            guessingResult = input("My guess is %s. If right, write 'right', if too low, write 'low', if too high, write 'high':" % str(numberGuessed))
+            if guessingResult == "right":
+                print("Hooray")
+                print ("It took me %s guesses" % str(counter))
+                break
+            elif guessingResult == "low":
+                del possibilities[0:middle]
+            elif guessingResult == "high":
+                del possibilities[middle:]
+
+if __name__=="__main__":
+    main()
