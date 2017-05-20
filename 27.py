2d28577be2c9c4d344b30cf00a075a0ddde0f65e
zseen <43151120+zseen@users.noreply.github.com>
2017-05-20 21:04:24 +0100

    Ex 27 - Add 'Game' class, delet parameter 'board', handle exceptions

--- 27.py
+++ 27.py
@@ -4,48 +4,67 @@
 #   b. actual board
 # 3. [X] refactor to askForInput(character) with character being 'x' or 'o'
 # 4. [X] introduce counter, break loop when 9
-# 5. create a game class with the board being a member, main is only 2 lines: game = Game() \ game.play()
+# 5. [X]create a game class with the board being a member, main is only 2 lines: game = Game() \ game.play()
 # 6. do not allow overwriting any mark
 # 7. handle all kinds of exceptions separately, with useful error messages
 
-def askForInput(board, mark):
+# [X]delete board param, use self.board
 
-    while True:
-        try:
-            inputString=input("%s, it is your turn! Type the row and column (1-3) where you would like to put your %s in this format: r,c.:" % (mark, mark) )
-            inputList=inputString.split(",")
+class Game(object):
 
-            r = int(inputList[0])
-            c = int(inputList[1])
-            if board[r - 1][c - 1] == 0:
-                board[r - 1][c - 1] = mark
-                break
-            else:
-                print("Ohh, this field is already marked.")
-        except Exception as e:
-            print("Ohh, exception: " + str(e))
 
-def draw(board):
-    N=3
-    for j in range(0, N):
-        for i in range(0, N):
-            print(board[j][i], end=" ")
+    def __init__(self):
+        self.board = [[0, 0, 0],
+                 [0, 0, 0],
+                 [0, 0, 0]]
 
-        print("")
+    def draw(self):
+        N = 3
+        for j in range(0, N):
+            for i in range(0, N):
+                print(self.board[j][i], end=" ")
+
+            print("")
+
+
+    def askForInput(self, mark):
+        while True:
+            try:
+                inputString=input("%s, it is your turn! Type the row and column (1-3) where you would like to put your %s in this format: r,c.:" % (mark, mark) )
+                inputList=inputString.split(",")
+
+                r = int(inputList[0])
+                c = int(inputList[1])
+                if  len(inputList) != 2:
+                    print("Ohh, tooo many numbers")
+                elif self.board[r - 1][c - 1] == 0:
+                    self.board[r - 1][c - 1] = mark
+                    break
+                else:
+                    print("Ohh, this field is already marked.")
+            except ValueError as ve:
+                print("Ohh, a ValueError")
+            except IndexError as ie:
+                print("Ohh, an IndexError, please check if your numbers are <= 3")
+            except NameError as ne:
+                print("Ohh, a NameError")
+            # except Exception as e:
+            #     print("Ohh, exception: " + str(e))
+
+    def play(self):
+        counter = 0
+        while counter <= 9:
+            self.askForInput("X")
+            self.draw()
+            self.askForInput("Y")
+            self.draw()
+            counter += 1
+
 
 def main():
-    board = [[0, 0, 0],
-             [0, 0, 0],
-             [0, 0, 0]]
-    counter=0
+    g=Game()
+    g.play()
 
-    while counter<=9:
-        askForInput(board, "X")
-        draw(board)
-        askForInput(board, "Y")
-        draw(board)
-        counter += 1
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
 
