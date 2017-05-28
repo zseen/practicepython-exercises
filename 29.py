32c06d1c1179ac2701ca2cbdcc6766fe8b1e3735
zseen <43151120+zseen@users.noreply.github.com>
2017-05-28 17:52:57 +0100

    Ex 29 - Refactor to smaller methods
    Introduce private and static methods, fix whitespaces, create enum.

--- 29.py
+++ 29.py
@@ -1,10 +1,47 @@
+from enum import Enum
+
+
 class Game(object):
+
+    class WinState(Enum):
+        FIRST_WINS = "First player wins"
+        SECOND_WINS = "Second player wins"
+        NOBODY_WINS = "Nobody wins"
 
     def __init__(self, size):
         self.size = size
-        self.board = [[0 for _ in range(self.size)] for _ in range(self.size)]
+        self.numFields = self.size ** 2
 
-    def draw(self):
+        self.board = None
+        self.counter = None
+
+    def play(self):
+        self.__resetBoard()
+        while True:
+            if self.__turnAndCheckGameOver('X'):
+                break
+
+            if self.__turnAndCheckGameOver('Y'):
+                break
+
+    # Private methods
+
+    def __resetBoard(self):
+        self.board = [[0 for _ in range(self.size)] for _ in range(self.size)]
+        self.counter = 0
+
+    def __turnAndCheckGameOver(self, mark):
+        self.__askForInput(mark)
+        self.__drawBoard()
+        if self.__isGameOver():
+            return True
+
+        self.counter += 1
+        if self.counter >= self.numFields:
+            print("Draw")
+            return True
+
+    def __drawBoard(self):
         for j in range(0, self.size):
             print(" ---" * self.size)
             print("| ", end="")
@@ -15,146 +52,115 @@ class Game(object):
         print(" ---" * self.size)
         print("")
 
-
-    def askForInput(self, mark):
+    def __askForInput(self, mark):
         while True:
             try:
-                inputString=input("%s, it is your turn! Type the row and column (1-3) where you would like to put your %s in this format: r,c.:" % (mark, mark) )
-                inputList=inputString.split(",")
+                inputString = input("%s, it is your turn! Type the row and column (1-3)"
+                                    " where you would like to put your %s in this format: r,c.:" % (mark, mark))
+                inputList = inputString.split(",")
 
                 r = int(inputList[0])
                 c = int(inputList[1])
-                if  len(inputList) != 2:
-                    print("Ohh, tooo many numbers")
+                if len(inputList) != 2:
+                    print("Ohh, too many numbers.")
                 elif r < 1 or c < 1:
-                    print ("Ohh, your numbers should be between 1 and 3")
+                    print("Ohh, your numbers should positive integers.")
                 elif self.board[r - 1][c - 1] == 0:
                     self.board[r - 1][c - 1] = mark
                     break
                 else:
                     print("Ohh, this field is already marked.")
             except ValueError as ve:
-                print("Ohh, a ValueError")
-            except IndexError as ie:
-                print("Ohh, an IndexError, please check if your numbers are <= 3")
+                print("Ohh, a ValueError: " + str(ve))
+            except IndexError:
+                print("Ohh, an IndexError, please check if your numbers are <= %s" % str(self.size))
             except NameError as ne:
-                print("Ohh, a NameError")
-            # except Exception as e:
-            #     print("Ohh, exception: " + str(e))
+                print("Ohh, a NameError: " + str(ne))
+            except Exception as e:
+                print("Ohh, Exception: " + str(e))
 
-    def winCheck(self):
-        horizontal=self.horizontalWin(self.board)
-        if horizontal != NOBODY_WINS:
+    def __winCheck(self):
+        horizontal = self.__horizontalWin()
+        if horizontal != Game.WinState.NOBODY_WINS:
             return horizontal
 
-        vertical=self.verticalWin(self.board)
-        if vertical != NOBODY_WINS:
+        vertical = self.__verticalWin()
+        if vertical != Game.WinState.NOBODY_WINS:
             return vertical
 
-        diagonal=self.diagonalWin(self.board)
-        if diagonal != NOBODY_WINS:
-            return diagonal
+        return self.__diagonalWin()
 
-        return NOBODY_WINS
-
-    def isGameOver(self):
-        if self.winCheck() == FIRST_WINS:
-            print(FIRST_WINS)
+    def __isGameOver(self):
+        if self.__winCheck() == Game.WinState.FIRST_WINS:
+            print(Game.WinState.FIRST_WINS.value)
             return True
-        elif self.winCheck() == SECOND_WINS:
-            print(SECOND_WINS)
+
+        if self.__winCheck() == Game.WinState.SECOND_WINS:
+            print(Game.WinState.SECOND_WINS.value)
             return True
-        elif self.winCheck() == NOBODY_WINS:
-            return False
 
+        return False
 
-
-
-    def play(self):
-        counter = 0
-        while True:
-            self.askForInput("X")
-            self.draw()
-
-            if self.isGameOver() == True:
-                break
-
-            counter += 1
-
-            if counter >= 9:
-                print("Draw")
-                break
-
-            self.askForInput("Y")
-            self.draw()
-            if self.isGameOver() == True:
-                break
-            counter += 1
-
-
-    def numberToWinner(self,mark):
+    @staticmethod
+    def __markToWinner(mark):
         if mark == 'X':
-            return FIRST_WINS
+            return Game.WinState.FIRST_WINS
         elif mark == 'Y':
-            return SECOND_WINS
+            return Game.WinState.SECOND_WINS
         else:
             raise ValueError("Unexpected winner")
-
-    def all_same(self,items):
+        
+    @staticmethod
+    def __allSame(items):
         return all(x == items[0] for x in items)
 
-    def horizontalWin(self,board):
-        N = len(board)
+    def __horizontalWin(self):
+        N = len(self.board)
 
         for j in range(0, N):
             rowList = []
             for i in range(0, N):
-                rowList.append(board[j][i])
+                rowList.append(self.board[j][i])
 
-            if (self.all_same(rowList)) and rowList[0] != 0:
-                return self.numberToWinner(rowList[0])
+            if (Game.__allSame(rowList)) and rowList[0] != 0:
+                return Game.__markToWinner(rowList[0])
 
-        return NOBODY_WINS
+        return Game.WinState.NOBODY_WINS
 
-    def verticalWin(self,board):
-        N = len(board)
+    def __verticalWin(self):
+        N = len(self.board)
 
         for i in range(0, N):
-            rowList = []
+            columnList = []
             for j in range(0, N):
-                rowList.append(board[j][i])
+                columnList.append(self.board[j][i])
 
-            if (self.all_same(rowList)) and rowList[0] != 0:
-                return self.numberToWinner(rowList[0])
+            if (Game.__allSame(columnList)) and columnList[0] != 0:
+                return Game.__markToWinner(columnList[0])
 
-        return NOBODY_WINS
+        return Game.WinState.NOBODY_WINS
 
-    def diagonalWin(self,board):
-        N = len(board)
-        rowList = []
+    def __diagonalWin(self):
+        N = len(self.board)
+        mainDiagonalList = []
         for i in range(0, N):
-            rowList.append(board[i][i])
+            mainDiagonalList.append(self.board[i][i])
 
-        if (self.all_same(rowList)) and rowList[0] != 0:
-            return self.numberToWinner(rowList[0])
+        if (Game.__allSame(mainDiagonalList)) and mainDiagonalList[0] != 0:
+            return Game.__markToWinner(mainDiagonalList[0])
 
-        rowList = []
+        secondaryDiagonalList = []
         for i in range(0, N):
-            rowList.append(board[i][N - 1 - i])
+            secondaryDiagonalList.append(self.board[i][N - 1 - i])
 
-        if (self.all_same(rowList)) and rowList[0] != 0:
-            return self.numberToWinner(rowList[0])
+        if (Game.__allSame(secondaryDiagonalList)) and secondaryDiagonalList[0] != 0:
+            return Game.__markToWinner(secondaryDiagonalList[0])
 
-        return NOBODY_WINS
-
-FIRST_WINS = "First player wins"
-SECOND_WINS = "Second player wins"
-NOBODY_WINS = "Nobody won"
-
+        return Game.WinState.NOBODY_WINS
 
 
 def main():
-    g=Game(3)
+    g = Game(3)
     g.play()
 
 if __name__ == "__main__":
