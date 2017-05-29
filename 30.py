3527e5f50064bdc6aa04282d95ce9d002ebe3561
zseen <43151120+zseen@users.noreply.github.com>
2017-05-29 14:46:45 +0100

    Ex 30 - Refactor this exercise with a class

--- 30.py
+++ 30.py
@@ -1,24 +1,25 @@
 import random
-from random import randint
 
 
-def readSOWPODS():
-    text = []
-    with open('SOWPODS.txt', 'r') as f:
-       line = f.readline().strip()
-       text.append(line)
-       while line:
+class readAFile(object):
+
+    @staticmethod
+    def readSOWPODS():
+        text = []
+        with open('SOWPODS.txt', 'r') as f:
             line = f.readline().strip()
             text.append(line)
-    return text[random.randint(0, (len(text) - 1))]
-
-
+            while line:
+                line = f.readline().strip()
+                text.append(line)
+        return text[random.randint(0, (len(text) - 1))]
 
 
 def main():
-    randomWord = readSOWPODS()
-    print(randomWord)
+    r = readAFile()
+    print(r.readSOWPODS())
 
-if __name__=="__main__":
+
+if __name__== "__main__":
     main()
 
