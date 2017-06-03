2fc67207bbd9280176e9a664ca141e1241e9ab9e
zseen <43151120+zseen@users.noreply.github.com>
2017-06-03 17:25:46 +0100

    Ex 31 - Add exercise 31 without handling any problems (like already guessed letters)

--- 31.py
+++ 31.py
@@ -1,56 +1,72 @@
 import random
 
+
 class Hangman(object):
-
-
     def __init__(self):
         self.randomWord = None
+        self.charRandomWord = []
+        self.askForLetter = None
         self.underscoreList = []
-
+        self.strCharRandomWord = None
+        self.strPrintUnderscore = None
+        self.strUnderscoreList = None
 
     def getAWord(self):
-        textForWord = []
         with open('SOWPODS.txt', 'r') as f:
-            line = f.readline().strip()
-            textForWord.append(line)
-            while line:
-                line = f.readline().strip
-                textForWord.append(line)
-
-        randomIndex = random.randint(0, (len(textForWord)-1))
+            line = f.readlines()
+            textForWord = [x.strip() for x in line]
+        randomIndex = random.randint(0, len(textForWord) - 1)
         self.randomWord = textForWord[randomIndex]
+        print(self.randomWord)
+
+        self.charRandomWord = list(self.randomWord)
+        self.strCharRandomWord = ",".join(self.charRandomWord)
+        print(self.strCharRandomWord)
 
     def getAnInput(self):
-        askForLetter = input(str("Give me a letter please!:"))
+        self.askForLetter = input(str('Give me a letter please!:'))
 
-
-    def letterInWord(self):
-        self.randomWord
-        self.charRandomWord = self.randomWord.split(" ")
-
-        for item in self.charRandomWord:
-            self.printUnderscore()
-            if self.getAnInput() not in self.charRandomWord:
-                return False
-
-
-        index = 0
-        for _ in range(0, len(self.charRandomWord)):
-            if self.charRandomWord[index] == self.getAnInput():
-                del.self.underscoreList[index]
-                self.underscoreList.insert(index, self.getAnInput())
+        # print(self.askForLetter)
+        # return self.askForLetter
 
     def printUnderscore(self):
         for i in range(0, len(self.charRandomWord)):
-            self.underscoreList.append(_)
-        return self.underscoreList
+            self.underscoreList.append("_")
+        self.strPrintUnderscore = " ".join(self.underscoreList)
+        print(self.strPrintUnderscore)
+
+    def underscoreOrLetter(self):
+        for index in range(0, len(self.charRandomWord)):
+            if self.askForLetter == self.charRandomWord[index]:
+                del self.underscoreList[index]
+                self.underscoreList.insert(index, self.askForLetter)
+                self.strUnderscoreList = " ".join(self.underscoreList)
+
+        print(self.strUnderscoreList)
+
+        # print(self.underscoreList)
+
+    # def letterInWord(self):
+    #     for item in range(0, len(self.randomWord)):
+    #         if self.askForLetter == self.charRandomWord[item]:
+    #             print(self.askForLetter, end='')
+    #         else:
+    #             print("_ ", end='')
 
     def playHangman(self):
-        while
+        counter = 0
         self.getAWord()
-        self.getAnInput()
-        self.letterInWord()
+        # x = self.printUnderscore()
+        self.printUnderscore()
+        while counter <= len(self.charRandomWord):
 
+            self.getAnInput()
+            # self.letterInWord()
+            self.underscoreOrLetter()
+            if self.underscoreList == self.charRandomWord:
+                print("Yeeee, you won!")
+                break
+            counter += 1
 
 
 def main():
@@ -58,3 +74,5 @@ def main():
     h.playHangman()
 
 
+if __name__ == '__main__':
+    main()
