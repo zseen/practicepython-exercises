01ac3091925ab511cc95765322dea1f6e89cf167
zseen <43151120+zseen@users.noreply.github.com>
2017-06-18 15:13:06 +0100

    Ex 35 - Refactor this exercise: uses a function and a main

--- 35.py
+++ 35.py
@@ -1,15 +1,23 @@
 import json
 from collections import Counter
 
-with open("info.json", "r") as f:
-    birthdaysDict = json.load(f)
 
-months = []
-for key, value in birthdaysDict.items():
-    splitValue = value.split(" ")
-    month = splitValue[len(splitValue)-1]
-    months.append(month)
+def monthsCounter():
+    with open("info.json", "r") as f:
+        birthdaysDict = json.load(f)
+    months = []
+    for key, value in birthdaysDict.items():
+        splitValue = value.split(" ")
+        month = splitValue[len(splitValue)-1]
+        months.append(month)
+    return Counter(months)
 
-countMonths = Counter(months)
-print(countMonths)
+
+def main():
+    countTheMonths = monthsCounter()
+    print(countTheMonths)
+
+if __name__ == "__main__":
+    main()
+
 
