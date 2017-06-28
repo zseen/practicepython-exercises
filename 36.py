9105c6922dc471ae5d4b99db68ef250943ce04a6
zseen <43151120+zseen@users.noreply.github.com>
2017-06-28 21:08:06 +0100

    Ex 36 - Refactor this exercise
    - simplify import
    - remove member
    - add new class: PlotData
    - add a method which creates the data (it returns a PlotData object)

--- 36.py
+++ 36.py
@@ -4,46 +4,55 @@
 # 3. Create a class PlotData, which has two fields: xAxis and yAxis
 # 4. Refactor data creation to a method, which returns a PlotData object
 
-from bokeh.plotting import figure, show, output_file
+import bokeh.plotting as bk
 import json
-from collections import Counter
+import collections
+
+
+class PlotData(object):
+
+    def __init__(self):
+        self.xAxis = []
+        self.yAxis = []
+
 
 class MonthFigure(object):
     ALL_MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
                   "November", "December", "Unknown"]
 
     def __init__(self):
-        self.months = []
-        self.birthdaysDict = {}
-        self.month = []
+        pass
 
     def countTheMonths(self):
         with open("info.json", "r") as f:
-            self.birthdaysDict = json.load(f)
+            birthdaysDict = json.load(f)
 
-        for key, value in self.birthdaysDict.items():
+        months = []
+        for key, value in birthdaysDict.items():
             splitValue = value.split(" ")
-            self.month = splitValue[len(splitValue) - 1]
-            self.months.append(self.month)
+            month = splitValue[len(splitValue) - 1]
+            months.append(month)
 
-        return Counter(self.months)
+        return collections.Counter(months)
 
     def showFigure(self):
+        data = self.getData()
+        self.drawPlot(data)
+
+    def drawPlot(self, data):
+        bk.output_file("plot.html")
+        p = bk.figure(x_range=MonthFigure.ALL_MONTHS)
+        p.vbar(x=data.xAxis, top=data.yAxis, width=0.5)
+        bk.show(p)
+
+    def getData(self):
         monthsCounter = self.countTheMonths()
-
-        x = []             # ["Jan", "Feb", "March", "Unknown"]
-        y = []              # [3,     3,     2,       1]
-
+        data = PlotData()
         for key, value in monthsCounter.items():
-            x.append(key)
-            y.append(value)
+            data.xAxis.append(key)
+            data.yAxis.append(value)
+        return data
 
-        output_file("plot.html")
-
-        p = figure(x_range=MonthFigure.ALL_MONTHS)
-        p.vbar(x=x, top=y, width=0.5)
-
-        show(p)
 
 def main():
     m = MonthFigure()
